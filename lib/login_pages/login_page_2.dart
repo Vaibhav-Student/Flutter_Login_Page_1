05608da909e2523394981cdae26a592358c5e9ed
Vaibhav-Student <vaibhavbeladiya1112@gmail.com>
2025-04-13 17:35:28 +0530

    update in login page 2

--- lib/login_pages/login_page_2.dart
+++ lib/login_pages/login_page_2.dart
@@ -82,244 +82,256 @@ class _LoginPage2State extends State<LoginPage2> {
 
       ),
       body: Container(
-        width: MediaQuery.of(context).size.width, 
-        height: MediaQuery.of(context).size.height, 
-        decoration: const BoxDecoration(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
           gradient: LinearGradient(
             begin: Alignment.topCenter,
             end: Alignment.bottomCenter,
             colors: [
-              Color.fromARGB(255, 145, 172, 235),  // Blue gradient start
-              Color.fromARGB(255, 68, 122, 231),  // Blue gradient middle
-              Color(0xFF1CB5E0),  // Cyan gradient end
+              Color.fromARGB(255, 142, 167, 226),
+              Color.fromARGB(255, 102, 188, 228),
+              Color.fromARGB(255, 53, 163, 214),
             ],
           ),
-          
         ),
-        child: SafeArea(
-          child: SingleChildScrollView(
-            padding: const EdgeInsets.all(24.0),
-            child: Card(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20),
-              ),
-              elevation: 8,
-              child: Padding(
-                padding: const EdgeInsets.all(24.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    // Logo
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Image.asset(
-                          'lib/assets/Images/logoipsum.png',  // Add your logo image
-                          height: 40,
+        child: Center(
+          child: SafeArea(
+            child: SingleChildScrollView(
+              padding: EdgeInsets.all(24.0),
+              child: Column(
+                children: [
+          
+                  const SizedBox(height: 15),
+          
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset(
+                        'lib/assets/Images/logoipsum.png',
+                        height: 40,
                         ),
-                        const SizedBox(width: 8),
+                        const SizedBox(width: 8),   
                         Text(
                           'Logoipsum',
                           style: GoogleFonts.inter(
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold,
+                            fontSize: 32,
+                            fontWeight: FontWeight.bold
                           ),
                         ),
-                      ],
+                    ],
+                  ),  
+          
+                  SizedBox(height: 24), 
+          
+                  Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20.0),
                     ),
-                    const SizedBox(height: 32),
-                    
-                    // Login Text
-                    Text(
-                      'Login',
-                      style: GoogleFonts.inter(
-                        fontSize: 28,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    const SizedBox(height: 8),
-                    
-                    // Don't have an account text
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          "Don't have an account? ",
-                          style: GoogleFonts.inter(color: Colors.grey[600]),
-                        ),
-                        TextButton(
-                          onPressed: () {},
-                          child: Text(
-                            'Sign Up',
+                    elevation: 8,
+                    child: Padding(
+                      padding: const EdgeInsets.all(24.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                                  
+                          Text(
+                            'Login',
                             style: GoogleFonts.inter(
-                              color: Colors.blue[700],
-                              fontWeight: FontWeight.w600,
+                              fontSize: 28,
+                              fontWeight: FontWeight.bold,
                             ),
                           ),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(height: 25),
-
-                    // Email Field
-                    TextField(
-                      decoration: InputDecoration(
-                        labelText: 'Email',
-                        hintText: 'Loisbecket@gmail.com',
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(12),
-                        ),
-                        contentPadding: const EdgeInsets.all(16),
-                        suffixIcon: Icon(
-                          Icons.email,
-                          color: Colors.grey,
-                        ),
-                        ),
-                      ),
-                    
-                    const SizedBox(height: 16),
-
-                    // Password Field
-                    TextField(
-                      obscureText: !_isPasswordVisible,
-                      decoration: InputDecoration(
-                        labelText: 'Password',
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(12),
-                        ),
-                        contentPadding: const EdgeInsets.all(16),
-                        suffixIcon: IconButton(
-                          icon: Icon(
-                            _isPasswordVisible ? Icons.visibility : Icons.visibility_off,
-                            color: Colors.grey,
+                  
+                          const SizedBox(height: 8,),
+                  
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                "Don't have an account?",
+                                style: GoogleFonts.inter(
+                                  color: Colors.grey[600],
+                                ),
+                              ),
+                              TextButton(
+                                onPressed: () {},
+                                child: Text(
+                                  'Sign Up',
+                                  style: GoogleFonts.inter(
+                                    color: Colors.blue[700],
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                              ),
+                            ],
                           ),
-                          onPressed: () {
-                            setState(() {
-                              _isPasswordVisible = !_isPasswordVisible;
-                            });
-                          },
-                        ),
-                      ),
-                    ),
-
-                    // Remember me and Forgot Password
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Row(
-                          children: [
-                            Checkbox(
-                              value: _rememberMe,
-                              onChanged: (value) {
-                                setState(() {
-                                  _rememberMe = value ?? false;
-                                });
-                              },
+          
+                          TextField(
+                            decoration: InputDecoration(
+                              labelText: 'Email',
+                              hintText: 'Vaibhav123@gmain.com',
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(12),
+                              ),   
+                              contentPadding: const EdgeInsets.all(16), 
                             ),
-                            Text(
-                              'Remember me',
-                              style: GoogleFonts.inter(
-                                fontSize: 14,
+                          ),
+          
+                          SizedBox(height: 10,),
+          
+                          TextField(
+                            obscureText: !_isPasswordVisible,
+                            decoration: InputDecoration(
+                              labelText: 'Password',
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(12),
+                              ),
+                              contentPadding: const EdgeInsets.all(16),
+                              suffixIcon: IconButton(
+                                icon: Icon(
+                                  _isPasswordVisible ? Icons.visibility : Icons.visibility_off,
+                                  color: Colors.grey,
+                                ), onPressed: () {
+                                  setState(() {
+                                    _isPasswordVisible = !_isPasswordVisible;
+                                  });
+                                },
                               ),
                             ),
-                          ],
-                        ),
-                        TextButton(
-                          onPressed: () {},
-                          child: Text(
-                            'Forgot Password?',
-                            style: GoogleFonts.inter(
-                              color: Colors.blue[700],
-                              fontSize: 13,
+                          ),
+          
+                          const SizedBox(height: 10,),
+          
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Row(
+                                children: [
+                                  Checkbox(
+                                    value: _rememberMe,
+                                    onChanged: (value) {
+                                      setState(() {
+                                        _rememberMe = value ?? false;
+                                      });
+                                    },
+                                  ),
+                                  Text(
+                                    'Remember me',
+                                    style: GoogleFonts.inter(
+                                      fontSize: 14,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              TextButton(
+                                onPressed: () {},
+                                child: Text(
+                                  'Forgot Password',
+                                  style: GoogleFonts.inter(
+                                  color: Colors.blue[700],
+                                  fontSize: 13
+                                ),
+                                ),
+                              ),
+                            ],
+                          ),
+          
+                          ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.blue[700],
+                              minimumSize: const Size(double.infinity, 50),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(12),
+                              ),
+                            ),
+                            child: Text(
+                              "Login In",
+                              style: GoogleFonts.inter(
+                                color: Colors.white,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w600,
+                              ),
                             ),
                           ),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(height: 24),
-
-                    // Login Button
-                    ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.blue[700],
-                        minimumSize: const Size(double.infinity, 50),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(12),
-                        ),
-                      ),
-                      child: Text(
-                        'Log In',
-                        style: GoogleFonts.inter(
-                          color: Colors.white,
-                          fontSize: 16,
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                    ),
-                    const SizedBox(height: 24),
-
-                    // Or divider
-                    Row(
-                      children: [
-                        Expanded(child: Divider(color: Colors.grey[300])),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 16),
-                          child: Text(
-                            'Or',
-                            style: GoogleFonts.inter(color: Colors.grey[600]),
+          
+                          const SizedBox(height: 24,),
+          
+                          Row(
+                            children: [
+                              Expanded(
+                                child: Divider(
+                                  color: Colors.grey[300],
+                                ),
+                              ),
+                              Padding(
+                                padding: (const EdgeInsets.symmetric(horizontal: 16)),
+                                child: Text(
+                                  'Or',
+                                  style: GoogleFonts.inter(
+                                    color: Colors.grey[600],
+                                  ),
+                                ),
+                              ),
+                              Expanded(
+                                child: Divider(
+                                  color: Colors.grey[300],
+                                ),
+                              ),
+                            ],
                           ),
-                        ),
-                        Expanded(child: Divider(color: Colors.grey[300])),
-                      ],
+          
+                          const SizedBox(height: 24),
+          
+                          OutlinedButton.icon(
+                            onPressed: () {},
+                            icon: Image.asset(
+                              'lib/assets/Images/Google_logo.png',
+                              height: 24,
+                            ),
+                            label: Text(
+                              'Containe with Google',
+                              style: GoogleFonts.inter(),
+                            ),
+                            style: OutlinedButton.styleFrom(
+                              minimumSize: const Size(double.infinity, 50),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(12)
+                              ),
+                            ),
+                          ),
+                          
+                          const SizedBox(height: 24),
+          
+                          OutlinedButton.icon(
+                            onPressed: () {},
+                            icon: Image.asset(
+                              'lib/assets/Images/Facebook_logo.png',
+                              height: 24,
+                            ),
+                            label: Text(
+                              'Containe with Facebook',
+                              style: GoogleFonts.inter(),
+                            ),
+                            style: OutlinedButton.styleFrom(
+                              minimumSize: const Size(double.infinity, 50),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(12)
+                              ),
+                            ),
+                          ),
+                  
+                        ],
+                      ),
                     ),
-                    const SizedBox(height: 24),
-
-                    // Social Login Buttons
-                    OutlinedButton.icon(
-                      onPressed: () {},
-                      icon: Image.asset(
-                        'lib/assets/Images/Google_logo.png',
-                        height: 24,
-                      ),
-                      label: Text(
-                        'Continue with Google',
-                        style: GoogleFonts.inter(),
-                      ),
-                      style: OutlinedButton.styleFrom(
-                        minimumSize: const Size(double.infinity, 50),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(12),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(height: 16),
-                    OutlinedButton.icon(
-                      onPressed: () {},
-                      icon: Image.asset(
-                        'lib/assets/Images/Facebook_logo.png',
-                        height: 24,
-                      ),
-                      label: Text(
-                        'Continue with Facebook',
-                        style: GoogleFonts.inter(),
-                      ),
-                      style: OutlinedButton.styleFrom(
-                        minimumSize: const Size(double.infinity, 50),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(12),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
           ),
         ),
       ),
-
       
     );
   }
